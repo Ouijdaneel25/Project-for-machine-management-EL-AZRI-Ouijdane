789351f69fcb71d6fa1fb09d7d49f4476fcfcbc2
Ouijdaneel25 <60515139+Ouijdaneel25@users.noreply.github.com>
2022-01-21 18:34:28 +0100

    Update Connexion.java

--- src/main/java/connexion/Connexion.java
+++ src/main/java/connexion/Connexion.java
@@ -7,8 +7,7 @@ import java.sql.SQLException;
 public class Connexion {
 
     private static String login = "qmcdzpctqdiweq";
-    private static String password = "ad2aa790656390d317dd2c3fd296ffcbf711d786d3b8ce1d93108c023608c000
-";
+    private static String password = "ad2aa790656390d317dd2c3fd296ffcbf711d786d3b8ce1d93108c023608c000";
     private static String url = "jdbc:mysql://ec2-3-222-49-168.compute-1.amazonaws.com/dcifuoonpgeinc?sslmode=require";
     private Connection connection = null;
     private static Connexion instane;
